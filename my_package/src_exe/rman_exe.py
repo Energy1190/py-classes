455815243ef2df81ea20cac9080cdad894bbd63e
Energy1190 <energyneo0@gmail.com>
2019-06-13 22:05:12 +0300

    v 0.22.23

--- my_package/src_exe/rman_exe.py
+++ my_package/src_exe/rman_exe.py
@@ -4,26 +4,29 @@ import datetime
 from traceback import format_exc
 from my_package.classes.rman_api import QueryParser
 
+
 def pop_arg(name, array):
     result = None
     try:
         n = array.index(name)
-        result = array.pop(n+1)
+        result = array.pop(n + 1)
         array.pop(n)
     except:
         pass
 
     return result
 
+
 def collect_kwargs(array):
     result = {}
     for item in array:
         if "--KWARG_" in item:
             name = item.replace("--KWARG_")
-            value = pop_arg(item,array)
+            value = pop_arg(item, array)
             result[name] = value
 
     return result
+
 
 if __name__ == '__main__':
     date = datetime.datetime.now().strftime("%d.%m.%Y-%H:%M")
@@ -31,14 +34,14 @@ if __name__ == '__main__':
     try:
         parse = sys.argv
         kwargs = collect_kwargs(parse)
-        debug = pop_arg('--debug',parse)
+        debug = pop_arg('--debug', parse)
         exe_location = os.sep.join(parse.pop(0).split(os.sep)[:-1])
-        cls = QueryParser(parse,parse,debug=debug,workdir=exe_location)
-        rman,action = cls.create()
-        rman.run(action,**kwargs)
+        cls = QueryParser(parse, debug=debug, workdir=exe_location)
+        rman, action = cls.create()
+        rman.run(action, **kwargs)
         rman.close()
     except:
-        filename = os.path.join(exe_location,'critical_error_report_{}'.format(date))
-        stream = open(filename,'w')
+        filename = os.path.join(exe_location, 'critical_error_report_{}'.format(date))
+        stream = open(filename, 'w')
         stream.write(format_exc())
         stream.close()
